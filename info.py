33a8ac6e9d598701a575f0060c8d37e64f0ea24c
J. Nick Koston <nick@koston.org>
2022-11-26 16:28:21 -1000

    feat: add first version

---
+++ info.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+
+from usb_devices import BluetoothDevice
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("usb_devices").setLevel(logging.DEBUG)
+
+
+async def run() -> None:
+    loop = asyncio.get_running_loop()
+    for i in range(0, 9):
+        dev = BluetoothDevice(i)
+        try:
+            await loop.run_in_executor(None, dev.setup)
+        except FileNotFoundError:
+            print(f"hci{i} not found")
+            continue
+        assert dev.usb_device is not None  # nosec
+        print(
+            f"hci{i} manufacturer: {dev.usb_device.manufacturer}, "
+            f"product: {dev.usb_device.product}, "
+            f"vendor_id: {dev.usb_device.vendor_id}, "
+            f"product_id: {dev.usb_device.product_id}, "
+            f"bus_id: {dev.usb_device.bus_id}, "
+            f"dev_num: {dev.usb_device.dev_num}"
+        )
+
+
+asyncio.run(run())
